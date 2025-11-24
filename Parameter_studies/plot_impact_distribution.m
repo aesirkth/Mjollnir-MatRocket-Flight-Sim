c740d78eff42f898faecd469e6583909203eb719
Harald <trojbom.h@gmail.com>
2025-11-24 18:51:12 +0100

    Map labels and minor adjustments to montecarlosim geomapping

--- Parameter_studies/plot_impact_distribution.m
+++ Parameter_studies/plot_impact_distribution.m
@@ -22,16 +22,19 @@ figure;
 wgs84 = wgs84Ellipsoid('meters');
 geobasemap satellite
 
-% Launch site coordinates
+% Launch site
 lat0 = 67.889663108;
 lon0 = 21.10416625;
 alt0 = 341;
+geoscatter(lat0, lon0, 30, 'g', 'filled');
+text(lat0, lon0, ' Launch site', 'VerticalAlignment','bottom', 'HorizontalAlignment','left', 'Color','w');
+hold on;
 
 % Impact zone A
 latA = [67.8723008, 67.8723084, 67.8723660, 67.8780572, 67.8801491, 67.8821042, 67.8832564, 67.8844700, 67.9101134, 67.9532620, 67.9761755, 67.9173745, 67.8996103, 67.8723008];
 longA = [21.0393914, 21.0515890, 21.1438720, 21.1521476, 21.1521790, 21.1562466, 21.1614810, 21.1614749, 21.1987985, 21.1982443, 20.9897495, 20.9880485, 21.0275520, 21.0393914];
-geoplot(latA, longA, 'b', 'LineWidth', 2);
-hold on;
+geoplot(latA, longA,'--', LineWidth=1);
+text(67.9101134, 21.1987985, ' Impact zone A', 'VerticalAlignment','bottom', 'HorizontalAlignment','left', 'Color','w');
 
 % Convert ENU coordinates to geodetic
 [lat, lon, alt] = enu2geodetic(impact_positions(1,:), impact_positions(2,:), zeros(1, size(impact_positions,2)), lat0, lon0, alt0, wgs84);
