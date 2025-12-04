10799455173f8b275cdf9bd0c985119f4f6a8c2f
Harald <trojbom.h@gmail.com>
2025-12-04 12:49:33 +0100

    minor changes

--- Parameter_studies/plot_impact_distribution.m
+++ Parameter_studies/plot_impact_distribution.m
@@ -4,18 +4,18 @@ end
 impact_positions = cellfun(@(h) h.position(:,:,end), rocket_historians(1:sim_nr-2) , 'UniformOutput', false);
 impact_positions = cell2mat(impact_positions);
 
-%pdf = multivariable_normal_distribution(impact_positions(1:2,:));
+pdf = multivariable_normal_distribution(impact_positions(1:2,:));
 
-%[X,Y] = meshgrid(-10000:100:10000,-10000:100:10000);
-%x = [reshape(X,[],numel(X));reshape(Y,[], numel(Y))];
+[X,Y] = meshgrid(-10000:100:10000,-10000:100:10000);
+x = [reshape(X,[],numel(X));reshape(Y,[], numel(Y))];
 
-%p = pdf(x);
+p = pdf(x);
 
-%P = reshape(p, size(X));
+P = reshape(p, size(X));
 
-%contour3(X,Y,P, 1e-10:3e-9:1e-7); 
+contour3(X,Y,P, 1e-10:3e-9:1e-7); 
 %hold on; view([0,90]); xlim([-6000 6000]);ylim([-6000 6000]);
-%surf(X,Y,P);
+surf(X,Y,P);
 
 % Geomapping
 figure;
@@ -41,5 +41,3 @@ text(67.9101134, 21.1987985, ' Impact zone A', 'VerticalAlignment','bottom', 'Ho
 
 % Plot impact points
 geoscatter(lat, lon, 30, 'r');
-
-%scatter(impact_positions(1,:), impact_positions(2,:));
