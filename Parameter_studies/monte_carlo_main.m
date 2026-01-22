3a050ebfae9966668e5667698db800f406976e51
alexsoproni <alex.soproni@aesir.se>
2026-01-22 13:27:06 +0100

    added error message in try catch

--- Parameter_studies/monte_carlo_main.m
+++ Parameter_studies/monte_carlo_main.m
@@ -1,14 +1,18 @@
 setup;
 
+data = load(".\Rocket_source\Wind_Data\wind.mat", "tables");
+wind_tables = data.tables;
+
 if isfile(".\Parameter_studies\monte_carlo.mat")
-load(".\Parameter_studies\monte_carlo.mat")
+load(".\Parameter_studies\monte_carlo.mat");
+
 else
 
 my_rocket = Mjollnir();
 
 job = struct(); job.t_max = 270;
 
-%% Paramters to vary:
+%% Parameters to vary:
 
 thrust_force = 2700:10:4400;
 
@@ -20,23 +24,15 @@ max_sims = 1e3;
 
 rocket_historians = cell(1,max_sims);
 
-
-
-
 save(".\Parameter_studies\monte_carlo.mat", '-v7.3')
 end
 
 
-wind_data = load("test.mat");
-wind_data.tables = trimdata(wind_data.tables, 2025);
-
-
-while sim_nr < max_sims
+while sim_nr <= max_sims
 clear atmosphere_model
 try
-
-my_rocket.atmosphere.dataset = wind_data.tables{randi(2025)};
-disp(":)")
+idx = randi(numel(wind_tables));
+my_rocket.atmosphere.dataset                   = wind_tables{idx};
 my_rocket.engine.thrust_force                  = thrust_force(randi(numel(thrust_force)));
 my_rocket.aerodynamics.drag_coefficient_offset = drag_coefficient_offset(randi(numel(drag_coefficient_offset)));
 
@@ -44,7 +40,9 @@ rocket_historians{sim_nr} = run_simulation(my_rocket, job);
 
 sim_nr = sim_nr +1;
 save(".\Parameter_studies\monte_carlo.mat", '-v7.3')
-catch
+catch ME
+    builtin('warning', 'MonteCarlo:SimulationFailed', ...
+        'Simulation %d failed:\n%s', sim_nr, getReport(ME, 'extended', 'hyperlinks', 'off'));
 end
 
 end
